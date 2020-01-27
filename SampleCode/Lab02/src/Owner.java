898ced95c68078b8e32106003b08bf1f51ac30eb
disensee <isenseed@students.westerntc.edu>
2020-01-26 18:12:03 -0600

    Started to set up lab02

---
+++ SampleCode/Lab02/src/Owner.java
@@ -0,0 +1,47 @@
+
+public class Owner {
+	//Instance variables
+	private String firstName;
+	private String lastName;
+	private Portfolio portfolio;
+	
+	//Constructor
+	public Owner(String firstName, String lastName, Portfolio portfolio) {
+		
+	}
+	
+	//Methods
+	public void assignPortfolioToOwner(Portfolio p) {
+		
+	}
+	
+	
+	
+	//Getters/setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Portfolio getPortfolio() {
+		return portfolio;
+	}
+
+	public void setPortfolio(Portfolio portfolio) {
+		this.portfolio = portfolio;
+	}
+	
+	
+	
+}
